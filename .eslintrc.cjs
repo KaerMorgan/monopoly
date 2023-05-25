1e9973317b0aa3c99f56de487bd36c6ee454fa3d
Danil Filatov <SubDante@gmail.com>
2023-05-25 22:14:48 +0300

    task-18# setup lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,24 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
-}
+  plugins: ["react-refresh", "prettier"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+  },
+};
